35bcc09334cb1139b34ecbc12d18ec9ca2d83921
E Folan <folanenda@gmail.com>
2018-10-08 18:11:30 -0400

    connect to SQLDB in Azure

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,22 @@ namespace DotNetCoreSqlDb
             // Add framework services.
             services.AddMvc();
 
+            //use sql db if in azure otherwise use sqlite
+
+            if(Environment.
+            GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == ("Production"))
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+            
+            options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            
+            else
+            
+            services.AddDbContext<MyDatabaseContext>(options => 
+            
+            options.UseSqlite("DataSource=localdatabase.db"));
+            
+            //Automatically perform database migration
+            services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
